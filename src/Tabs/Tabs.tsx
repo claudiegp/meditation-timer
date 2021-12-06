7113a030c918a9afccfc8fcbed4ae66e18f26be9
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2021-12-06 15:26:03 +0000

    feat(tabs): add state to Tabs component

--- src/Tabs/Tabs.tsx
+++ src/Tabs/Tabs.tsx
@@ -1,10 +1,18 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import { Tab, Tabs as TabGroup } from "@material-ui/core";
 
-export const Tabs: FunctionComponent = () => (
-  <TabGroup className="Tab-group">
-    <Tab id="Timer" label="Timer" />
-    <Tab id="Stopwatch" label="Stopwatch" />
-  </TabGroup>
-);
+export const Tabs: FunctionComponent = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: any, newValue: number) => {
+    // todo: add type for event
+    setValue(newValue);
+  };
+  return (
+    <TabGroup className="Tab-group" value={value} onChange={handleChange}>
+      <Tab id="Timer" label="Timer" />
+      <Tab id="Stopwatch" label="Stopwatch" />
+    </TabGroup>
+  );
+};
