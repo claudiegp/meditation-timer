a4f25305239424cbbe0f53b10e3147f8e31161d5
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2021-12-20 14:59:37 +0000

    feat(tabs): set state of clockType on click of tab

--- src/Tabs/Tabs.tsx
+++ src/Tabs/Tabs.tsx
@@ -1,18 +1,22 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 
 import { Tab, Tabs as TabGroup } from "@material-ui/core";
 
-export const Tabs: FunctionComponent = () => {
-  const [value, setValue] = useState(0);
+interface TabsProps {
+  clockType: string;
+  onChange: (e: any, clockType: string) => void;
+}
 
-  const handleChange = (event: any, newValue: number) => {
-    // todo: add type for event
-    setValue(newValue);
-  };
+export const Tabs: FunctionComponent<TabsProps> = ({ clockType, onChange }) => {
   return (
-    <TabGroup className="Tab-group" value={value} onChange={handleChange}>
-      <Tab id="Timer" label="Timer" />
-      <Tab id="Stopwatch" label="Stopwatch" />
+    <TabGroup
+      className="Tab-group"
+      value={clockType}
+      onChange={onChange}
+      centered
+    >
+      <Tab id="Timer" label="Timer" value="timer" />
+      <Tab id="Stopwatch" label="Stopwatch" value="stopwatch" />
     </TabGroup>
   );
 };
