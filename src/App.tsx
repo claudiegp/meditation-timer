8ad2469b7c5097eeb6f629463cf2efd3b65cd8f5
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2021-12-20 16:58:30 +0000

    feat(stopwatch): refactor to lift state up into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,130 @@
-import React, { useState } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import "./App.css";
 import "./Timer.css";
-import { Container } from "@material-ui/core";
+import { Container } from "@mui/material";
 import { Tabs } from "./Tabs/Tabs";
-import TimeContainer from "./TimeContainer/TimeContainer";
 import Header from "./Header/Header";
+import TimeSelector from "./TimeSelector/TimeSelector";
+import { Clock } from "./Clock/Clock";
+import ButtonBanner from "./ButtonBanner/ButtonBanner";
+import { calculateTotalSeconds } from "./Utilities/time_utility";
 
 export default function App(): JSX.Element {
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
   const [clockType, setClockType] = useState("timer");
+  const [totalSeconds, setTotalSeconds] = useState<number>(0);
+  const [hasCounterStarted, setHasCounterStarted] = useState<boolean>(false);
+  const intervalRef: { current: NodeJS.Timeout | null } = useRef(null);
+  const [hasCounterPaused, setHasCounterPaused] = useState<boolean>(false);
 
-  const handleChange = (event: any, clockType: string) => {
-    // todo: add type for event
+  /* sets and updates time */
+  useEffect(() => {
+    setTotalSeconds(calculateTotalSeconds(hours, minutes, seconds));
+  }, [hours, minutes, seconds]);
+
+  const handleHoursChange = (event: any) => {
+    setHours(parseInt(event.target.value));
+  };
+
+  const handleMinutesChange = (event: any) => {
+    setMinutes(parseInt(event.target.value));
+  };
+
+  const handleSecondsChange = (event: any) => {
+    setSeconds(parseInt(event.target.value));
+  };
+
+  const startCountdown = () => {
+    if (clockType === "timer" && totalSeconds > 0) {
+      const intervalID: NodeJS.Timeout = setInterval(
+        () => setTotalSeconds((totalSeconds: number) => totalSeconds - 1),
+        1000
+      );
+      intervalRef.current = intervalID;
+      setHasCounterStarted(true);
+      setHasCounterPaused(false);
+
+      return () => clearInterval(intervalID);
+    }
+  };
+
+  const startCountup = () => {
+    const intervalID: NodeJS.Timeout = setInterval(
+      () => setTotalSeconds((totalSeconds: number) => totalSeconds + 1),
+      1000
+    );
+    intervalRef.current = intervalID;
+    setHasCounterStarted(true);
+
+    if (totalSeconds === 3599) {
+      pauseCounter();
+    }
+
+    return () => clearInterval(intervalID);
+  };
+
+  const pauseCounter = () => {
+    clearInterval(intervalRef.current as NodeJS.Timeout);
+    setHasCounterPaused(true);
+  };
+  const resetInputValues = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const resetCounter = () => {
+    clearInterval(intervalRef.current as NodeJS.Timeout);
+    setHasCounterStarted(false);
+
+    setTotalSeconds(0);
+    resetInputValues();
+  };
+
+  const handleTabChange = (event: any, clockType: string) => {
     setClockType(clockType);
+    resetCounter();
   };
 
   return (
     <div className="App">
       <Header />
-      <Container className="Box">
-        <Tabs clockType={clockType} onChange={handleChange} />
-        <TimeContainer clockType={clockType} />
+      <Container>
+        <Tabs clockType={clockType} onChange={handleTabChange} />
+        <br></br>
+        <Clock totalSeconds={totalSeconds} />
+
+        {clockType === "timer" && (
+          <TimeSelector
+            hours={hours}
+            minutes={minutes}
+            seconds={seconds}
+            handleHoursChange={handleHoursChange}
+            handleMinutesChange={handleMinutesChange}
+            handleSecondsChange={handleSecondsChange}
+          />
+        )}
+
+        {clockType === "timer" && (
+          <ButtonBanner
+            startCountdown={startCountdown}
+            pauseCounter={pauseCounter}
+            resetCounter={resetCounter}
+            hasCounterStarted={hasCounterStarted}
+            hasCounterPaused={hasCounterPaused}
+          ></ButtonBanner>
+        )}
+        {clockType === "stopwatch" && (
+          <ButtonBanner
+            startCountdown={startCountup}
+            pauseCounter={pauseCounter}
+            resetCounter={resetCounter}
+            hasCounterStarted={hasCounterStarted}
+            hasCounterPaused={hasCounterPaused}
+          ></ButtonBanner>
+        )}
       </Container>
     </div>
   );
--- src/TimeSelector/TimeSelector.tsx
+++ src/TimeSelector/TimeSelector.tsx
@@ -1,165 +1,87 @@
 import { TextField, MenuItem } from "@material-ui/core";
 // todo update to use MUI
-import React, { FunctionComponent, useEffect, useRef, useState } from "react";
-import ButtonBanner from "../ButtonBanner/ButtonBanner";
-import { Clock } from "../Clock/Clock";
-import { calculateTotalSeconds } from "../Utilities/time_utility";
+import React, { FunctionComponent } from "react";
 
 const optionsHours: number[] = Array.from(Array(6).keys());
 const optionsMinutes: number[] = Array.from(Array(60).keys());
 const optionsSeconds: number[] = Array.from(Array(60).keys());
 
 interface TimeSelectorProps {
-  clockType: string;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  handleHoursChange: (event: any) => void;
+  handleMinutesChange: (event: any) => void;
+  handleSecondsChange: (event: any) => void;
 }
 
-const TimeSelector: FunctionComponent<TimeSelectorProps> = ({ clockType }) => {
-  const [hours, setHours] = useState<number>(0);
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(0);
-  const [totalSeconds, setTotalSeconds] = useState<number>(0);
-
-  const intervalRef: { current: NodeJS.Timeout | null } = useRef(null);
-  const [hasCounterStarted, setHasCounterStarted] = useState<boolean>(false);
-  const [hasCounterPaused, setHasCounterPaused] = useState<boolean>(false);
-
-  const startCountdown = () => {
-    if (clockType === "timer" && totalSeconds > 0) {
-      const intervalID: NodeJS.Timeout = setInterval(
-        () => setTotalSeconds((totalSeconds: number) => totalSeconds - 1),
-        1000
-      );
-      intervalRef.current = intervalID;
-      setHasCounterStarted(true);
-      setHasCounterPaused(false);
-
-      return () => clearInterval(intervalID);
-    }
-  };
-
-  const startCountup = () => {
-    const intervalID: NodeJS.Timeout = setInterval(
-      () => setTotalSeconds((totalSeconds: number) => totalSeconds + 1),
-      1000
-    );
-    intervalRef.current = intervalID;
-    setHasCounterStarted(true);
-
-    if (totalSeconds === 3599) {
-      pauseCounter();
-    }
-
-    return () => clearInterval(intervalID);
-  };
-
-  const pauseCounter = () => {
-    clearInterval(intervalRef.current as NodeJS.Timeout);
-    setHasCounterPaused(true);
-  };
-
-  const resetInputValues = () => {
-    setHours(0);
-    setMinutes(0);
-    setSeconds(0);
-  };
-
-  const resetCounter = () => {
-    clearInterval(intervalRef.current as NodeJS.Timeout);
-    setHasCounterStarted(false);
-
-    setTotalSeconds(0);
-    resetInputValues();
-  };
-
-  /* sets and updates time */
-  useEffect(() => {
-    setTotalSeconds(calculateTotalSeconds(hours, minutes, seconds));
-  }, [hours, minutes, seconds]);
-
+const TimeSelector: FunctionComponent<TimeSelectorProps> = ({
+  hours,
+  minutes,
+  seconds,
+  handleHoursChange,
+  handleMinutesChange,
+  handleSecondsChange,
+}) => {
   return (
     <>
-      <Clock totalSeconds={totalSeconds} />
+      <TextField
+        id="hours"
+        data-testid="menu-items-hours"
+        variant="filled"
+        defaultValue={0}
+        value={hours}
+        label="Hours"
+        onChange={handleHoursChange}
+        select
+        color={"primary"}
+      >
+        {optionsHours.map((option, id) => (
+          <MenuItem
+            key={option}
+            value={option}
+            data-testid={`menu-item-hours-${id}`}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <TextField
+        id="minutes"
+        data-testid="menu-items-minutes"
+        variant="filled"
+        defaultValue={0}
+        value={minutes}
+        label="Minutes"
+        onChange={handleMinutesChange}
+        select
+      >
+        {optionsMinutes.map((option) => (
+          <MenuItem key={option} value={option} id="menu-item-minutes">
+            {option}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <TextField
+        id="seconds"
+        data-testid="menu-items-seconds"
+        variant="filled"
+        defaultValue={0}
+        value={seconds}
+        label="Seconds"
+        onChange={handleSecondsChange}
+        select
+      >
+        {optionsSeconds.map((option) => (
+          <MenuItem key={option} value={option} id="menu-item-seconds">
+            {option}
+          </MenuItem>
+        ))}
+      </TextField>
       <br></br>
-
-      {clockType === "timer" && (
-        <>
-          <TextField
-            id="hours"
-            data-testid="menu-items-hours"
-            variant="filled"
-            defaultValue={0}
-            value={hours}
-            label="Hours"
-            onChange={(event) => setHours(parseInt(event.target.value))}
-            select
-            color={"primary"}
-          >
-            {optionsHours.map((option, id) => (
-              <MenuItem
-                key={option}
-                value={option}
-                data-testid={`menu-item-hours-${id}`}
-              >
-                {option}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          <TextField
-            id="minutes"
-            data-testid="menu-items-minutes"
-            variant="filled"
-            defaultValue={0}
-            value={minutes}
-            label="Minutes"
-            onChange={(event) => setMinutes(parseInt(event.target.value))}
-            select
-          >
-            {optionsMinutes.map((option) => (
-              <MenuItem key={option} value={option} id="menu-item-minutes">
-                {option}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          <TextField
-            id="seconds"
-            data-testid="menu-items-seconds"
-            variant="filled"
-            defaultValue={0}
-            value={seconds}
-            label="Seconds"
-            onChange={(event) => setSeconds(parseInt(event.target.value))}
-            select
-          >
-            {optionsSeconds.map((option) => (
-              <MenuItem key={option} value={option} id="menu-item-seconds">
-                {option}
-              </MenuItem>
-            ))}
-          </TextField>
-        </>
-      )}
-
-      {clockType === "timer" && (
-        <ButtonBanner
-          startCountdown={startCountdown}
-          pauseCounter={pauseCounter}
-          resetCounter={resetCounter}
-          hasCounterStarted={hasCounterStarted}
-          hasCounterPaused={hasCounterPaused}
-        ></ButtonBanner>
-      )}
-
-      {clockType === "stopwatch" && (
-        <ButtonBanner
-          startCountdown={startCountup}
-          pauseCounter={pauseCounter}
-          resetCounter={resetCounter}
-          hasCounterStarted={hasCounterStarted}
-          hasCounterPaused={hasCounterPaused}
-        ></ButtonBanner>
-      )}
+      <br></br>
     </>
   );
 };
