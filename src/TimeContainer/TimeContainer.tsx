f4a70adbbae174389f558eab87866e5810424d93
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2021-12-20 14:54:20 +0000

    feat(stopwatch): render based on clockType selected

--- src/TimeContainer/TimeContainer.tsx
+++ src/TimeContainer/TimeContainer.tsx
@@ -1,13 +1,22 @@
-import { Container } from "@material-ui/core";
 import React, { FunctionComponent } from "react";
 import TimeSelector from "../TimeSelector/TimeSelector";
 
-const TimeContainer: FunctionComponent = () => {
-  return (
-    <Container>
-      <TimeSelector />
-    </Container>
-  );
+interface TimeContainerProps {
+  clockType: string;
+}
+
+const TimeContainer: FunctionComponent<TimeContainerProps> = ({
+  clockType,
+}) => {
+  if (clockType === "timer") {
+    return <TimeSelector clockType={clockType} />;
+  }
+
+  if (clockType === "stopwatch") {
+    return <TimeSelector clockType={clockType} />;
+  }
+
+  return <>Select a clock type</>;
 };
 
 export default TimeContainer;
