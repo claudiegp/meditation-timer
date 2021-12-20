2e4e70244e4ce79d8956c36e86583e70c32bbc28
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2021-12-20 14:50:14 +0000

    feat(stopwatch): hide input fields

--- src/TimeSelector/__tests__/TimeSelector.test.tsx
+++ src/TimeSelector/__tests__/TimeSelector.test.tsx
@@ -1,13 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TimeSelector from "../TimeSelector";
 
-const renderComponent = () => {
-  render(<TimeSelector />);
+const renderTimer = () => {
+  render(<TimeSelector clockType="timer" />);
 };
-describe("TimeSelector", () => {
+
+// todo:
+const renderStopwatch = () => {
+  render(<TimeSelector clockType="stopwatch" />);
+};
+
+describe("TimeSelector when clockType is `timer`", () => {
   beforeEach(() => {
-    renderComponent();
+    renderTimer();
   });
   describe("rendering", () => {
     it("should render a start button", () => {
@@ -20,10 +26,21 @@ describe("TimeSelector", () => {
       expect(button).toBeInTheDocument();
     });
 
-    it("should render a pause button when user clicks `start`", () => {
+    // use async await to wait for the component to re-render
+    it("should render a pause button when user clicks `start`", async () => {
+      const hourInput = screen.getByTestId("menu-items-hours");
+      userEvent.click(hourInput);
+      const option = await screen.findByTestId("menu-items-hours-1");
+      userEvent.click(option);
+
+      // fireEvent.input(screen.getByLabelText("Hours"), {
+      //   target: { value: '{"x": "5"}' },
+      // });
+
       const startButton = screen.getByTestId("start-button");
       userEvent.click(startButton);
-      const pauseButton = screen.getByTestId("pause-button");
+
+      const pauseButton = await screen.findByTestId("pause-button");
       expect(pauseButton).toBeInTheDocument();
     });
 
@@ -40,10 +57,14 @@ describe("TimeSelector", () => {
     it("should allow user to select a start time from dropdown", () => {
       const hours = screen.getByLabelText(/hours/i);
       userEvent.click(hours);
+
+      // QUESTION - how to input a field
       // const x = screen.getByTestId("menu-item-hours-0");
-      // fireEvent.change(hours, { target: { value: 1 } });
+      fireEvent.change(hours, { target: { value: 1 } });
       // userEvent.selectOptions(hours, "1");
-      // expect(x).toEqual(1);
+
+      // check hours.value
+      expect(hours).toHaveValue(1);
     });
   });
 
@@ -55,12 +76,33 @@ describe("TimeSelector", () => {
       expect(startButton).toBeInTheDocument();
     });
   });
+  // describe("reset button", () => {
+  //   test("should reset the dropdown values back to 0", () => {
+  //     const zero = screen.getAllByText(/0/i);
+  //     expect(zero).toBeInTheDocument();
+  //   });
+  // });
+  // test("should reset the clock back to 0", () => {});
 });
 
-describe("reset button", () => {
-  test("should reset the dropdown values back to 0", () => {
-    const zero = screen.getAllByText(/0/i);
-    expect(zero).toBeInTheDocument();
+describe("TimeSelector when clockType is `stopwatch`", () => {
+  beforeEach(() => {
+    renderStopwatch();
+  });
+  describe("rendering", () => {
+    it("should not render `hours` input field", () => {
+      const inputField = screen.getByTestId("menu-items-hours");
+      expect(inputField).toBeInTheDocument();
+    });
+
+    it("should not render `minutes` input field", () => {
+      const inputField = screen.getByTestId("menu-items-minutes");
+      expect(inputField).toBeInTheDocument();
+    });
+
+    it("should not render `seconds` input field", () => {
+      const inputField = screen.getByTestId("menu-items-seconds");
+      expect(inputField).toBeInTheDocument();
+    });
   });
 });
-test("should reset the clock back to 0", () => {});
